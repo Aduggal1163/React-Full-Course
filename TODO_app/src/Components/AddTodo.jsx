8357f07f58405a2184b835787129e98466154ab9
Aduggal1163 <abhishekduggal04@gmail.com>
2025-01-28 11:08:09 +0530

    `Refactored AddTodo component to use useRef hook instead of useState for input fields`

--- TODO_app/src/Components/AddTodo.jsx
+++ TODO_app/src/Components/AddTodo.jsx
@@ -1,40 +1,54 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { IoMdAddCircle  } from "react-icons/io";
 export default function AddTodo({ onNewTodos }) {
-  const [todoName, setTodoName] = useState();
-  const [todoDate, setTodoDate] = useState();
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-    
-  };
-  const handleDateChange = (event) => {
-    setTodoDate(event.target.value);
-    
-  };
-  const handleSubmit=()=>{
+  // const [todoName, setTodoName] = useState();
+  // const [todoDate, setTodoDate] = useState();
+  // const noOfUpdates=useRef(0);
+  const todoNameElement=useRef();
+  const todoDateElement=useRef();
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  //   noOfUpdates.current+=1;
+  // };
+  // const handleDateChange = (event) => {
+  //   setTodoDate(event.target.value);
+  //   console.log(noOfUpdates.current);
+  // };
+  const handleSubmit=(e)=>{
+    const todoName=todoNameElement.current.value;
+    const todoDate=todoDateElement.current.value;
     onNewTodos(todoName,todoDate);
-    setTodoName("");
-    setTodoDate("");
+    todoNameElement.current.value="";
+    todoDateElement.current.value="";
+    e.preventDefault(); 
+    // setTodoName("");
+    // setTodoDate("");    
   }
   return (
     <div class="container text-center">
-      <div class="row ad-row">
+      <form class="row ad-row"
+            onSubmit={handleSubmit}>
         <div class="col-4">
-          <input type="text" value={todoName} onChange={handleNameChange} placeholder="Enter Todo Here" />
+          <input type="text" 
+          ref={todoNameElement}
+          // value={todoName} 
+          // onChange={handleNameChange} 
+          placeholder="Enter Todo Here" />
         </div>
         <div class="col-4">
-          <input type="date" value={todoDate} onChange={handleDateChange}/>
+          <input type="date"
+          ref={todoDateElement}
+          //  value={todoDate} onChange={handleDateChange}
+          />
         </div>
         <div class="col-2">
           <button
-            onClick={handleSubmit}
-            type="button"
             class="btn btn-success ad-button"
           >
             <IoMdAddCircle  />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
